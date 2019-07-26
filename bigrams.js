91b7460d104cedcbf8680377bc8c5bfd0011efbb
Jon Borgonia <jon@gomagames.com>
2019-07-25 19:07:39 -0700

    finished "Probably Spelled Correctly" -> bigrams.js

---
+++ bigrams.js
@@ -0,0 +1,23 @@
+const { stdin, stdout } = process
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: stdin,
+  output: stdout,
+  terminal: false
+});
+
+const BIGRAMS_LENGTH = 50
+
+const bigrams = []
+const cases = []
+
+rl.on('line', line => bigrams.length < BIGRAMS_LENGTH-1 ? bigrams.push(line.substr(0,2).toUpperCase()) : cases.push(line.toUpperCase()))
+
+rl.on('close', () => console.log(
+  cases.map(word =>
+    new Array(word.length - 1)
+      .fill(null)
+      .map((_,i) => word.substr(i, 2))
+      .every(bigrams.includes.bind(bigrams))
+  ).join('\n')
+))
