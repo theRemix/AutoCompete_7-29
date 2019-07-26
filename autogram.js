82e50bddc8dd152d0e999a5e307d4f0d554caee8
Jon Borgonia <jon@gomagames.com>
2019-07-25 19:54:01 -0700

    fix autogram stdin

--- autogram.js
+++ autogram.js
@@ -1,13 +1,1 @@
-const { stdin, stdout } = process
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: stdin,
-  output: stdout,
-  terminal: false
-});
-
-rl.on('line', () => null)
-
-rl.on('close', () => {
-  console.log('This sentence includes one a')
-})
+console.log('This sentence includes one a')
